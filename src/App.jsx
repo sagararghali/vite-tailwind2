cc1d86c511207751c5cecb4d099907b42ca3c928
Sagar Arghali Magar. <121717245+sagararghali@users.noreply.github.com>
2025-06-05 23:11:02 +1000

    app update

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Homebody from './Components/Homebody';
 export default function App() {
   return (
     <>
-    <div className=" min-h-screen bg-black bg-cover bg-center  bg-[url('/background-gradient-green-tones_23-2148382072.avif')] ">
+    <div className=" min-h-screen bg-black bg-cover bg-center  bg-[url('background-gradient-green-tones_23-2148382072.avif')] ">
     <Navbar title=" Comic World"/>
     <Homebody/>
     </div>
