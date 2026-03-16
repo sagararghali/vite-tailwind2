3c62b5870b6511a44976b7708b7151c8c85712c7
Sagar Arghali Magar. <121717245+sagararghali@users.noreply.github.com>
2026-03-16 15:02:22 +1100

    i am back

--- src/Components/Price.jsx
+++ src/Components/Price.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 export default function Price() {
   return (
 <>
-<div className="flex gap-2 justify-center bg-black text-white p-4 ">
+<div className="relative flex w-[50%] gap-11 mt-3 rounded-xl justify-center items-center bg-black text-white p-4 ">
+
 <div className='grid'>
 <div className="">The Netflix you love for just $9.99.</div>
 <div className=''>Get our most affordable, ad-supported plan.</div>
