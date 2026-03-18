bdefe422c79cd7fecd7905d68c4e354e5cbb04f6
Sagar Arghali Magar. <121717245+sagararghali@users.noreply.github.com>
2026-03-18 18:00:41 +1100

    tag added

--- src/Components/Price.jsx
+++ src/Components/Price.jsx
@@ -3,15 +3,16 @@ import React from 'react'
 export default function Price() {
   return (
 <>
-<div className=" flex w-[70%] gap-[40%] mt-6  mx-auto rounded-xl justify-center items-center bg-black text-white p-4 ">
+<div className='group cursor-pointer'>
+<div className="group-hover:scale-[108%] duration-500 flex w-[70%] gap-[40%] mt-6  mx-auto rounded-xl justify-center items-center bg-black text-white p-4 ">
 
-<div className='grid'>
-<div className="">The Netflix you love for just $9.99.</div>
+<div className=''>
+<div className="">The Comic World you love for just $9.99.</div>
 <div className=''>Get our most affordable, ad-supported plan.</div>
 </div>
 
-<div className="bg-gray"> 
-  <button className="border rounded-md border-2 border-red-500 p-2">
+<div className=" cursor-pointer"> 
+  <button className="hover:bg-gray-900 border rounded-md border-2 border-green-400 p-2">
 
   Learn More 
 
@@ -19,6 +20,6 @@ export default function Price() {
   </div>
 
 </div>
-
+</div>
 </>  )
 }
